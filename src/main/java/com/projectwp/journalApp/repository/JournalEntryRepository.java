b3900689fc5146212ae8799c255910e66d5cd10b
Piyush-1394 <piyushlodhi40@gmail.com>
2025-09-18 20:27:25 +0530

    Repository logic ( database )

---
+++ src/main/java/com/projectwp/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,11 @@
+package com.projectwp.journalApp.repository;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.projectwp.journalApp.entity.JournalEntry;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId>{
+	
+}
+	
