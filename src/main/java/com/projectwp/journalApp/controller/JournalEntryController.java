ac07bcc1a4c48c8d81ecfa5bc0d02ff3fb5fe9a9
Piyush-1394 <piyushlodhi40@gmail.com>
2025-10-16 15:49:19 +0530

    Added exception handling classes for graceful termination

--- src/main/java/com/projectwp/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/projectwp/journalApp/controller/JournalEntryController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,8 +31,8 @@ public class JournalEntryController {
 	}
 	
 	@GetMapping("/find/{id}")
-	public void findById(@PathVariable ObjectId id){
-		journalEntryService.findById(id);
+	public JournalEntry findById(@PathVariable ObjectId id){
+		return journalEntryService.findById(id);
 	}
 	
 	@GetMapping
--- src/main/java/com/projectwp/journalApp/service/JournalEntryService.java
+++ src/main/java/com/projectwp/journalApp/service/JournalEntryService.java
@@ -4,10 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import com.projectwp.journalApp.exception.ResourceNotFoundException;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import com.projectwp.journalApp.entity.JournalEntry;
@@ -32,20 +31,21 @@ public class JournalEntryService {
 		journalEntryRepository.deleteById(id);
 	}
 	
-	public ResponseEntity<JournalEntry> findById(ObjectId id) {
+	public JournalEntry findById(ObjectId id) {
 	   Optional<JournalEntry> journalEntry = journalEntryRepository.findById(id);
 	   if(journalEntry.isPresent()) {
-		   return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+
+		   return journalEntry.get();
+	   }else {
+		   throw new ResourceNotFoundException("journal entry not found with id " + id);
 	   }
-	   return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 	
 	public JournalEntry updateJournalEntryById(ObjectId id, JournalEntry newEntry) {
-		JournalEntry old = journalEntryRepository.findById(id).orElse(null);
-		if(old != null) {
-			old.setTitle(old.getTitle() !=  null && old.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
-			old.setContent(old.getContent()!= null && old.getContent().equals("") ? newEntry.getContent() : old.getContent());
-		}
+		JournalEntry old = journalEntryRepository.findById(id).orElseThrow(() ->  new ResourceNotFoundException("journal not found with id " + id));
+		old.setTitle(newEntry.getTitle() !=  null && !newEntry.getTitle().isEmpty() ? newEntry.getTitle() : old.getTitle());
+		old.setContent(newEntry.getContent()!= null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : old.getContent());
+		old.setDate(LocalDateTime.now());
 		journalEntryRepository.save(old);
 		return old;
 	}
